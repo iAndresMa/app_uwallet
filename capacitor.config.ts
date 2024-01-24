71f7c284ff9622f436427046b26f0468bd8c90fd
andreslopezuni <andres.lopez.m@uniminuto.edu>
2024-01-24 10:30:40 -0500

    Configuraciones adicionales para el proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app_uwallet',
+  appId: 'edu.uniminuto.uwallet',
+  appName: 'UWallet',
   webDir: 'www',
   server: {
     androidScheme: 'https'
