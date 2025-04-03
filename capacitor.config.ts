387a52ab27b0c51329931480106a8e50064845e3
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-04-03 08:19:54 -0500

    Se configura un splash screen para ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,22 +7,33 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'https',
     iosScheme: 'https',
-    hostname: "127.0.0.1",
-		cleartext: true,
-		allowNavigation: ["*"]
+    hostname: '127.0.0.1',
+    cleartext: true,
+    allowNavigation: ['*'],
   },
   plugins: {
+    SplashScreen: {
+      launchAutoHide: false,
+      launchShowDuration: 2000, // Opcional: duración en milisegundosandroid
+      SplashResourceName: 'splash', // Nombre del recurso en Androidios
+      SplashIdentifier: 'LaunchScreen', // Storyboard en
+      iOSbackgroundColor: '#ffffff',
+      showSpinner: true, // Opcional: muestra un
+      androidSpinnerStyle: 'large', // Opcional: estilo del spinner en Android
+      iosSpinnerStyle: 'large', // Opcional: estilo del spinner en iOS
+      spinnerColor: '#000000', // Opcional: color del spinner
+    },
     PrivacyScreen: {
       enable: false,
-      imageName: "Splashscreen"
+      imageName: 'Splashscreen',
     },
     StatusBar: {
-      backgroundColor: "#FBD342",
-    }
+      backgroundColor: '#FBD342',
+    },
   },
   ios: {
-    contentInset: "always",
-  }
+    contentInset: 'always',
+  },
 };
 
 export default config;
