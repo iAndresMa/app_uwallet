ec2378a522d0482b58d6c3f23e7b5b92f881255a
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-08-05 12:19:14 -0500

    Se coloca el (;) dentro de las interfaces

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,279 +1,280 @@
 export interface menuPrincipal {
-    icon: string,
-    name: string,
-    redirectTo: string
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
 
 export interface userUniminuto {
-    id: string,
-    Id: string,
-    Cn: string,
-    DeparmetNumber: string,
-    Descripcion: string,
-    DisplayName: string,
-    EmployeType: string,
-    FirstName: string,
-    GidNumber: string,
-    HomeDirectory: string,
-    HomePhone: string,
-    L: string,
-    LastName: string,
-    Mail: string,
-    Pager: string,
-    Sn: string,
-    St: string,
-    Street: string,
-    TelephoneNumber: string,
-    Title: string,
-    Uid: string,
-    UidNumber: string,
-    useraccountcontrol: string
+  id: string;
+  Id: string;
+  Cn: string;
+  DeparmetNumber: string;
+  Descripcion: string;
+  DisplayName: string;
+  EmployeType: string;
+  FirstName: string;
+  GidNumber: string;
+  HomeDirectory: string;
+  HomePhone: string;
+  L: string;
+  LastName: string;
+  Mail: string;
+  Pager: string;
+  Sn: string;
+  St: string;
+  Street: string;
+  TelephoneNumber: string;
+  Title: string;
+  Uid: string;
+  UidNumber: string;
+  useraccountcontrol: string;
 }
 
 export interface fotografia {
-    resp: string,
-    base: string,
-    fotografia: string
+  resp: string;
+  base: string;
+  fotografia: string;
 }
 
 export interface bytte {
-    code: string,
-    body: string
+  code: string;
+  body: string;
 }
 
 export interface cupoUsuario {
-    documento: string,
-    lugar: string,
-    cupo: string,
-    dia: string,
-    jornada: string,
-    message: string
+  documento: string;
+  lugar: string;
+  cupo: string;
+  dia: string;
+  jornada: string;
+  message: string;
 }
 
 export interface rectoria {
-    rectorias: rectoriaDatos
+  rectorias: rectoriaDatos;
 }
 
 interface rectoriaDatos {
-    id: string,
-    cod: string,
-    nombre: string
+  id: string;
+  cod: string;
+  nombre: string;
 }
 
 export interface sede {
-    sede: sedeDatos
+  sede: sedeDatos;
 }
 
 interface sedeDatos {
-    id: string,
-    nombre: string
+  id: string;
+  nombre: string;
 }
 
 export interface cupoBici {
-    respuesta: string
+  respuesta: string;
 }
 
 export interface solicitudBici {
-    "1": datosSolicitud
+  '1': datosSolicitud;
 }
 
 interface datosSolicitud {
-    "response_code": string,
-    "message": string
+  response_code: string;
+  message: string;
 }
 
 export interface eventos {
-    "0": eventoGeneal
+  '0': eventoGeneal;
 }
 
 interface eventoGeneal {
-    idplan: string,
-    idinstancia: string,
-    actividad: string,
-    rectoria: string,
-    sede: string,
-    modalidad: string,
-    estudiante: string,
-    famservidoradm: string,
-    estudianteconve: string,
-    padreestudiante: string,
-    egresado: string,
-    servidoradm: string,
-    profesor: string,
-    famprofesor: string,
-    hijoservidoradm: string,
-    fechaactividad: string,
-    invitado: string,
-    famestudiante: string,
-    visitante: string,
-    hijoestudiante: string,
-    hijoprofesor: string
+  idplan: string;
+  idinstancia: string;
+  actividad: string;
+  rectoria: string;
+  sede: string;
+  modalidad: string;
+  estudiante: string;
+  famservidoradm: string;
+  estudianteconve: string;
+  padreestudiante: string;
+  egresado: string;
+  servidoradm: string;
+  profesor: string;
+  famprofesor: string;
+  hijoservidoradm: string;
+  fechaactividad: string;
+  invitado: string;
+  famestudiante: string;
+  visitante: string;
+  hijoestudiante: string;
+  hijoprofesor: string;
 }
 
 export interface evento {
-    "0": eventoUnico
+  '0': eventoUnico;
 }
 
 interface eventoUnico {
-    idejeactividad: string,
-    nmproyecto: string,
-    codproyecto: string,
-    nmactividad: string,
-    codactividad: string,
-    categoria: string,
-    fechaactividad: string,
-    horaactividad: string,
-    finicioins: string,
-    ffinins: string,
-    objetivo: string,
-    tpresovir: string,
-    inscripcionprev: string,
-    encuesta: string,
-    lugarCanal: string,
-    respoblacion: string,
-    respresupuesto: string,
-    estudiante: string,
-    metaes: string,
-    profesor: string,
-    metapr: string,
-    servidoradm: string,
-    metasa: string,
-    egresado: string,
-    metaeg: string,
-    estudianteconve: string,
-    metaec: string,
-    hijoestudiante: string,
-    metahes: string,
-    hijoprofesor: string,
-    metahpr: string,
-    hijoservidoradm: string,
-    metahsa: string,
-    famestudiante: string,
-    metafes: string,
-    famprofesor: string,
-    metafpr: string,
-    famservidoradm: string,
-    metafsa: string,
-    padreestudiante: string,
-    metapes: string,
-    invitado: string,
-    metainv: string,
-    visitante: string,
-    metavis: string,
-    totalpmeta: string,
-    idinstancia: string
+  idejeactividad: string;
+  nmproyecto: string;
+  codproyecto: string;
+  nmactividad: string;
+  codactividad: string;
+  categoria: string;
+  fechaactividad: string;
+  horaactividad: string;
+  finicioins: string;
+  ffinins: string;
+  objetivo: string;
+  tpresovir: string;
+  inscripcionprev: string;
+  encuesta: string;
+  lugarCanal: string;
+  respoblacion: string;
+  respresupuesto: string;
+  estudiante: string;
+  metaes: string;
+  profesor: string;
+  metapr: string;
+  servidoradm: string;
+  metasa: string;
+  egresado: string;
+  metaeg: string;
+  estudianteconve: string;
+  metaec: string;
+  hijoestudiante: string;
+  metahes: string;
+  hijoprofesor: string;
+  metahpr: string;
+  hijoservidoradm: string;
+  metahsa: string;
+  famestudiante: string;
+  metafes: string;
+  famprofesor: string;
+  metafpr: string;
+  famservidoradm: string;
+  metafsa: string;
+  padreestudiante: string;
+  metapes: string;
+  invitado: string;
+  metainv: string;
+  visitante: string;
+  metavis: string;
+  totalpmeta: string;
+  idinstancia: string;
 }
 
 export interface eventosUwallet {
-    "0": eventosUsuario
+  '0': eventosUsuario;
 }
 
 interface eventosUsuario {
-    resp: string,
-    evento: string,
-    nombre: string,
-    idRecord: string
+  resp: string;
+  evento: string;
+  nombre: string;
+  idRecord: string;
 }
 
 export interface eventoInsrito {
-    resp: string,
-    evento: string,
-    nombre: string,
-    idRecord: string,
-    qr: string
+  resp: string;
+  evento: string;
+  nombre: string;
+  idRecord: string;
+  qr: string;
 }
 
 export interface RespEventoSoftExpert {
-    recordKey: string,
-    participantes: Array<any>,
-    datosError: Array<any>
+  recordKey: string;
+  participantes: Array<any>;
+  datosError: Array<any>;
 }
 
 export interface RespEventoInscripcion {
-    "0": {
-        evento: string,
-        id: string,
-        idRecord: string,
-        nombre: string,
-        resp: boolean
-    }
+  '0': {
+    evento: string;
+    id: string;
+    idRecord: string;
+    nombre: string;
+    resp: boolean;
+  };
 }
 
 export interface RespEvento {
-    resp: boolean,
-    msg: string
+  resp: boolean;
+  msg: string;
 }
 
 export interface validarAdmin {
-    resp: boolean,
-    correo: string,
-    sistema: string
+  resp: boolean;
+  correo: string;
+  sistema: string;
 }
 
 export interface qr {
-    ok: boolean,
-    tokenQr: string
+  ok: boolean;
+  tokenQr: string;
+  mensaje: string;
 }
 
 export interface eventos {
-    "0": eventoGeneal
+  '0': eventoGeneal;
 }
 
 export interface evento {
-    "0": eventoUnico
+  '0': eventoUnico;
 }
 
 export interface permiso {
-    id: number,
-    nombre: string
+  id: number;
+  nombre: string;
 }
 
 export interface TipoDocumento {
-    resp: boolean,
-    id: number,
-    nombre: string,
-    abreviatura: string
+  resp: boolean;
+  id: number;
+  nombre: string;
+  abreviatura: string;
 }
 
 export interface permisos {
-    modulos: Array<permiso>,
-    infoCompleta: boolean
+  modulos: Array<permiso>;
+  infoCompleta: boolean;
 }
 
 export interface sedeRectoria {
-    resp: boolean
+  resp: boolean;
 }
 
 export interface userInfo {
-    sede: string,
-    rectoria: string,
-    idUniminuto: string
+  sede: string;
+  rectoria: string;
+  idUniminuto: string;
 }
 
 export interface arraySedeEstudiante {
-    data: Array<sedeEstudiantes>
+  data: Array<sedeEstudiantes>;
 }
 
 export interface sedeEstudiantes {
-    descRectoria: string,
-    sede: string,
-    sedeId: string,
+  descRectoria: string;
+  sede: string;
+  sedeId: string;
 }
 export interface areas {
-    id: string,
-    codigo: string,
-    descripcion: string
+  id: string;
+  codigo: string;
+  descripcion: string;
 }
 
 export interface arrayAreas {
-    areas: Array<areas>
+  areas: Array<areas>;
 }
 
 export interface ButtonReaders {
-    buttons: Array<ButtonReader>
+  buttons: Array<ButtonReader>;
 }
 
 export interface ButtonReader {
-    name: string,
-    id: string
+  name: string;
+  id: string;
 }
